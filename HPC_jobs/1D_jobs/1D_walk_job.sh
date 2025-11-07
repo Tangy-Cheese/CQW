62d5eec35ee4ceb8d92e2cfc6720005cfc46c4c6
Adam Tang <adamtang2004@hotmail.com>
2025-11-07 01:45:13 +0000

    changed directory paths to work lol

--- HPC_jobs/1D_jobs/1D_walk_job.sh
+++ HPC_jobs/1D_jobs/1D_walk_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=1D_walk
-#SBATCH --output=/../../output/logs/%x_output_%j.log
-#SBATCH --error=/../../output/logs/%x_error_%j.log
+#SBATCH --output=../../output/logs/%x_output_%j.log
+#SBATCH --error=../../output/logs/%x_error_%j.log
 #SBATCH --time=02:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -13,4 +13,4 @@ module load PyTorch/2.0-Miniconda3-4.12.0-Python-3.10.4
 module load GCC/11.3.0 OpenMPI/4.1.4 SciPy-bundle/2022.05
 module load GCCcore/11.3.0 Python/3.10.4
 
-python /src/CQW_1D/1D_walk.py 100 500
+python ../../src/CQW_1D/1D_walk.py 100 500
