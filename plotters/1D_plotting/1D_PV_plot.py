a4732897c22ddc80e90d49a82565200c30a5c078
Adam Tang <adamtang2004@hotmail.com>
2025-11-09 17:30:56 +0000

    FINALLY PLOT PV WORKS

--- plotters/1D_plotting/1D_PV_plot.py
+++ plotters/1D_plotting/1D_PV_plot.py
@@ -1,22 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 # Load data from CSV file
-df = pd.read_csv('participation_data_finale.csv')
+df = pd.read_csv('../../output/data/1D_PV.csv')
 
-# Define columns corresponding to time steps
-time_steps = ["t=25", "t=50", "t=100", "t=250", "t=500", "t=1000"]
+time_steps = df.columns[1:]
+list(time_steps)
 
-# Define different markers for each time step
-markers = ["o", "s", "D", "^", "v", ">"]
-lines = ["solid", "dashed", (0, (3, 1, 1, 1, 1, 1)), "dotted", "dashdot", (5, (10, 3))]
-colors = ["#000000", "#1f77b4", "#d62728", "#2ca02c", "#9467bd", "#ff7f0e"]
+t_dim = len(time_steps)
 
-# Number of samples (for error calculation)
-N = 2000
+colors = [f"#{random.randint(0, 0xFFFFFF):06x}" for _ in range(t_dim)]
 
-plt.figure(figsize=(11,7))
+# Number of realizations (for error calculation)
+j = 10
 
 legend_entries = []
 
@@ -24,28 +22,22 @@ legend_entries = []
 for i, t in enumerate(time_steps):
     mean_values = df[t]
     std_dev = np.std(mean_values, ddof=1)  # Sample standard deviation
-    error_bars = std_dev / np.sqrt(N)      # Standard error
+    error_bars = std_dev / np.sqrt(j)      # Standard error
     
     print(f"Standard deviation for {t}: {std_dev:.3f}")
     
-    plt.errorbar(df["Disorder Strength"], mean_values, yerr=error_bars, fmt=markers[i], linestyle=lines[i], 
-                 markersize=6, color=colors[i], linewidth=1.5, markerfacecolor=colors[i], capsize=4)
+    plt.errorbar(df["Disorder Strength"], mean_values, yerr=error_bars, fmt=".", linestyle="-", markersize=5, color=colors[i], linewidth=1.5, markerfacecolor=colors[i], capsize=4)
     
     legend_entries.append(f"{t}, σ={std_dev:.2f}")
 
-font = 14.4
-math_p =  r'$\mathcal{P}$'  # Unicode for 𝒫
+math_p =  r'$\mathcal{P}$' 
 # Add plot details
-plt.xlabel("W", fontsize=font)
-plt.ylabel(math_p + "(W)", fontsize=font)
-plt.legend(legend_entries, fontsize=font)
-
-# Remove gridlines
-plt.grid(False)
+plt.xlabel("W")
+plt.ylabel(math_p + "(W)")
+plt.legend(legend_entries)
 
 # Format ticks: inward-facing and same size on all four sides
-plt.tick_params(direction='in', length=6, width=1.2, labelsize=font, 
-                bottom=True, top=True, left=True, right=True)  # Major ticks on all sides
+plt.tick_params(direction='in', length=6, width=1.2, bottom=True, top=True, left=True, right=True)  # Major ticks on all sides
 
 # Set minor ticks on all sides
 plt.minorticks_on()
